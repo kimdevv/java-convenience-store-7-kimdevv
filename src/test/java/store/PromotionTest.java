8eb5e79eb61e18eb9ed494394a7accd600a3e977
kimdev <kim__dev@hufs.ac.kr>
2024-11-08 20:42:11 +0900

    test(Promotion): 파싱한 문자열로부터 프로모션을 등록하는 테스트를 정의하라

---
+++ src/test/java/store/PromotionTest.java
@@ -0,0 +1,27 @@
+package store;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import store.dto.ParsedPromotionDto;
+import store.utility.FileReader.PromotionFileReader;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class PromotionTest {
+    static List<ParsedPromotionDto> parsedPromotions;
+    static Promotions promotions;
+
+    @BeforeAll
+    static void 프로모션_목록을_파일로부터_가져온다() {
+        parsedPromotions = PromotionFileReader.inputPromotionsFromFile();
+    }
+
+    @BeforeEach
+    void 프로모션_목록을_등록한다() {
+        promotions = new Promotions();
+        promotions.setPromotionsFromParsedString(parsedPromotions);
+    }
+}
