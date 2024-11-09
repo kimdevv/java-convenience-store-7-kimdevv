0c8209f7437581b8f4fefc4ffa548934f9b19133
kimdev <kim__dev@hufs.ac.kr>
2024-11-09 19:38:47 +0900

    test(Utility): 구입 목록 입력을 파싱하는 테스트를 정의하라

--- src/test/java/store/utility/UtilityTest.java
+++ src/test/java/store/utility/UtilityTest.java
@@ -23,4 +23,14 @@ public class UtilityTest {
         List<ParsedPromotionDto> parsedPromotions = PromotionFileReader.inputPromotionsFromFile();
         assertThat(parsedPromotions.size() > 0);
     }
+
+    @Test
+    void 구매할_상품_목록을_입력하면_정해진_형식으로_파싱한다() {
+        List<BuyProductParseDto> result = BuyProductParser.parse("[콜라-5],[감자칩-1],[물-3]");
+
+        assertThat(result.size()).isEqualTo(3);
+        assertThat(result.get(0)).isEqualTo(new BuyProductParseDto("콜라", 5));
+        assertThat(result.get(1)).isEqualTo(new BuyProductParseDto("감자칩", 1));
+        assertThat(result.get(2)).isEqualTo(new BuyProductParseDto("물", 3));
+    }
 }
