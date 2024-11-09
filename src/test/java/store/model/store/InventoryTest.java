5fbdd88018d7c2225447465a08222abc92eab16c
kimdev <kim__dev@hufs.ac.kr>
2024-11-09 17:11:24 +0900

    test(Inventory): 상품을 구매하는 테스트를 정의하라

--- src/test/java/store/model/store/InventoryTest.java
+++ src/test/java/store/model/store/InventoryTest.java
@@ -32,4 +32,48 @@ public class InventoryTest {
     void Inventory_객체를_초기화한다() {
         inventory = new Inventory(allProducts);
     }
+
+    @Test
+    void 상품을_구매한다() {
+        BuyProductDto buyProductDto = inventory.buy("물", 5);
+        assertThat(buyProductDto.getName()).isEqualTo("물");
+        assertThat(buyProductDto.getQuantity()).isEqualTo(5);
+        assertThat(buyProductDto.getUnitPrice()).isEqualTo(500);
+        assertThat(buyProductDto.getFreeQuantity()).isEqualTo(0);
+        assertThat(buyProductDto.getLackQuantity()).isEqualTo(0);
+        assertThat(buyProductDto.getNeedQuantity()).isEqualTo(0);
+    }
+
+    @Test
+    void 프로모션_상품을_구매한다() {
+        BuyProductDto buyProductDto = inventory.buy("콜라", 6);
+        assertThat(buyProductDto.getName()).isEqualTo("콜라");
+        assertThat(buyProductDto.getQuantity()).isEqualTo(6);
+        assertThat(buyProductDto.getUnitPrice()).isEqualTo(1000);
+        assertThat(buyProductDto.getFreeQuantity()).isEqualTo(2);
+        assertThat(buyProductDto.getLackQuantity()).isEqualTo(0);
+        assertThat(buyProductDto.getNeedQuantity()).isEqualTo(0);
+    }
+
+    @Test
+    void 프로모션_상품을_부족하게구매한다() {
+        BuyProductDto buyProductDto = inventory.buy("콜라", 2);
+        assertThat(buyProductDto.getName()).isEqualTo("콜라");
+        assertThat(buyProductDto.getQuantity()).isEqualTo(2);
+        assertThat(buyProductDto.getUnitPrice()).isEqualTo(1000);
+        assertThat(buyProductDto.getFreeQuantity()).isEqualTo(0);
+        assertThat(buyProductDto.getLackQuantity()).isEqualTo(0);
+        assertThat(buyProductDto.getNeedQuantity()).isEqualTo(1);
+    }
+
+    @Test
+    void 프로모션_상품의_재고가_부족할_경우() {
+        BuyProductDto buyProductDto = inventory.buy("콜라", 15);
+        assertThat(buyProductDto.getName()).isEqualTo("콜라");
+        assertThat(buyProductDto.getQuantity()).isEqualTo(15);
+        assertThat(buyProductDto.getUnitPrice()).isEqualTo(1000);
+        assertThat(buyProductDto.getFreeQuantity()).isEqualTo(3);
+        assertThat(buyProductDto.getLackQuantity()).isEqualTo(6);
+        assertThat(buyProductDto.getNeedQuantity()).isEqualTo(0);
+    }
 }
