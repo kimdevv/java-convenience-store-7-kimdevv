78e75f6f2bbaa45dad9f5a4d0c33c6d0078c984f
kimdev <kim__dev@hufs.ac.kr>
2024-11-11 13:33:02 +0900

    test(Promotions): Promotions 객체에 대한 테스트를 진행하라

--- src/test/java/store/model/store/PromotionsTest.java
+++ src/test/java/store/model/store/PromotionsTest.java
@@ -2,7 +2,9 @@ package store.model.store;
 
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
 import store.dto.ParsedPromotionDto;
+import store.model.promotion.Promotion;
 import store.model.store.Promotions;
 import store.utility.FileReader.PromotionFileReader;
 
@@ -11,17 +13,31 @@ import java.util.List;
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class PromotionsTest {
-    static List<ParsedPromotionDto> parsedPromotions;
-    static Promotions promotions;
+    private static List<ParsedPromotionDto> parsedPromotions;
+    private static Promotions promotions;
 
     @BeforeAll
-    static void 프로모션_목록을_파일로부터_가져온다() {
+    public static void 프로모션_목록을_파일로부터_가져온다() {
         parsedPromotions = PromotionFileReader.inputPromotionsFromFile();
     }
 
     @BeforeEach
-    void 프로모션_목록을_등록한다() {
+    public void 프로모션_목록을_등록한다() {
         promotions = new Promotions();
         promotions.setPromotionsFromParsedString(parsedPromotions);
+    }
+
+    @Test
+    public void 이름으로_프로모션_객체를_검색한다() {
+        String promotionName = "탄산2+1";
+        Promotion foundPromotion = promotions.findPromotionByPromotionName(promotionName);
+        assertThat(foundPromotion.getName()).isEqualTo(promotionName);
+    }
+
+    @Test
+    public void 존재하지_않는_프로모션_이름으로_검색하면_null을_반환한다() {
+        String promotionName = "탄산5000+1";
+        Promotion foundPromotion = promotions.findPromotionByPromotionName(promotionName);
+        assertThat(foundPromotion).isNull();
     }
 }
